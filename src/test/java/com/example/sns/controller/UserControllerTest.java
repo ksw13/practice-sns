a963340356ceda7194ce6179f8b590b10c093137
ksw13 <ksw48483@gmail.com>
2023-03-22 17:43:46 +0900

    회원가입, 로그인 테스트 코드 작성
    가짜 entitiy 생성하는 fixture도 구현

---
+++ src/test/java/com/example/sns/controller/UserControllerTest.java
@@ -0,0 +1,120 @@
+package com.example.sns.controller;
+
+
+import com.example.sns.controller.request.UserJoinRequest;
+import com.example.sns.controller.request.UserLoginRequest;
+import com.example.sns.exception.SnsApplicationException;
+import com.example.sns.model.User;
+import com.example.sns.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+    @DisplayName("")
+    @Test
+    void 회원가입() throws Exception {
+        String userName = "userName";
+        String password = "password";
+
+        //join()을 호출하면 User return
+        when(userService.join(userName,password)).thenReturn(mock(User.class));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                // request body
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+        ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("")
+    @Test
+    void 회원가입시_이미_회원가입한_userName으로_가입한_경우() throws Exception {
+        String userName = "userName";
+        String password = "password";
+
+        //TODO Mocking
+        when(userService.join(userName,password)).thenThrow(new SnsApplicationException());
+
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void 로그인() throws Exception {
+        String userName = "userName";
+        String password = "password";
+
+        //join()을 호출하면 User return
+        when(userService.login(userName,password)).thenReturn("Test Token");
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // request body
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 로그인시_회원가입이_안된_userName을_입력할경우() throws Exception {
+        String userName = "userName";
+        String password = "password";
+
+        //join()을 호출하면 User return
+        when(userService.login(userName,password)).thenThrow(new SnsApplicationException());
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // request body
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void 로그인시_틀린_password를_입력할경우() throws Exception {
+        String userName = "userName";
+        String password = "password";
+
+        //join()을 호출하면 User return
+        when(userService.login(userName,password)).thenThrow(new SnsApplicationException());
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // request body
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+}
---
+++ src/test/java/com/example/sns/service/UserServiceTest.java
@@ -0,0 +1,95 @@
+package com.example.sns.service;
+
+import com.example.sns.exception.SnsApplicationException;
+import com.example.sns.fixture.UserEntityFixture;
+import com.example.sns.model.entity.UserEntity;
+import com.example.sns.repository.UserEntityRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootApplication
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserEntityRepository userEntityRepository;
+
+    @Test
+    void 회원가입이_정상적으로_동작() {
+        String userName = "userName";
+        String password = "password";
+
+        // mocking
+        // 회원가입이 된적이 없기 때문에 empty() 반환
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        when(userEntityRepository.save(any())).thenReturn(Optional.of(mock(UserEntity.class)));
+
+        Assertions.assertDoesNotThrow(() -> userService.join(userName,password));
+    }
+
+    @Test
+    void 회원가입시_userName으로_회원가입한적이_있는경우() {
+        String userName = "userName";
+        String password = "password";
+
+        // mocking
+        // 회원가입이 된적이 없기 때문에 empty() 반환
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(mock(UserEntity.class)));
+        when(userEntityRepository.save(any())).thenReturn(Optional.of(mock(UserEntity.class)));
+
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, () -> userService.join(userName, password));
+
+    }
+
+    @Test
+    void 로그인이_정상적으로_동작하는_경우() {
+        String userName = "userName";
+        String password = "password";
+
+        // Fixture로 가짜 entity 생성
+        UserEntity fixture = UserEntityFixture.get(userName, password);
+        // mocking
+        // 회원가입이 된적이 없기 때문에 empty() 반환
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+
+        Assertions.assertDoesNotThrow(()-> userService.login(userName,password));
+    }
+
+    @Test
+    void 로그인시_userName으로_회원가입한_유저가_없는경우() {
+        String userName = "userName";
+        String password = "password";
+
+        // mocking
+        // 회원가입이 된적이 없기 때문에 empty() 반환
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+
+        Assertions.assertDoesNotThrow(()-> userService.login(userName,password));
+    }
+
+    @Test
+    void 로그인시_password가_틀린경우() {
+        String userName = "userName";
+        String password = "password";
+        String wrongPassword = "wrong";
+
+        UserEntity fixture = UserEntityFixture.get(userName, password);
+
+        // mocking
+        // 회원가입이 된적이 없기 때문에 empty() 반환
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+
+        Assertions.assertThrows(SnsApplicationException.class, ()-> userService.login(userName,wrongPassword));
+    }
+}
