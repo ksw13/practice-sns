125d6f56d3d094372d230e594da6b3345f6227ea
ksw13 <ksw48483@gmail.com>
2023-03-28 13:57:55 +0900

    RuntimeException 처리

--- src/main/java/com/example/sns/exception/ErrorCode.java
+++ src/main/java/com/example/sns/exception/ErrorCode.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT,"userName is duplicated");
-
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT,"userName is duplicated"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR")
+    ;
 
     private HttpStatus status;
     private String message;
--- src/main/java/com/example/sns/exception/GlobalControllerAdvice.java
+++ src/main/java/com/example/sns/exception/GlobalControllerAdvice.java
@@ -2,6 +2,7 @@ package com.example.sns.exception;
 
 import com.example.sns.controller.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +16,15 @@ public class GlobalControllerAdvice {
     public ResponseEntity<?> applicationHandler(SnsApplicationException e) {
         log.error("Error occurs {}", e.toString());
         return  ResponseEntity.status(e.getErrorCode().getStatus())
+                //공통 응답 class Response 사용
                 .body(Response.error(e.getErrorCode().name()));
+    }
+
+    //SnsApplicationException 이외의 에러를 처리하기 위한 용도
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> applicationHandler(RuntimeException e) {
+        log.error("Error occurs {}", e.toString());
+        return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error(ErrorCode.INTERNAL_SERVER_ERROR.getMessage()));
     }
 }
