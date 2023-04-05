ca373ed3e8f7292be0c434a9f0ab2286d74a8d48
ksw13 <ksw48483@gmail.com>
2023-04-05 21:06:09 +0900

    사용자 인증을 위한 토큰 추가

--- src/main/java/com/example/sns/service/UserService.java
+++ src/main/java/com/example/sns/service/UserService.java
@@ -5,7 +5,9 @@ import com.example.sns.exception.SnsApplicationException;
 import com.example.sns.model.User;
 import com.example.sns.model.entity.UserEntity;
 import com.example.sns.repository.UserEntityRepository;
+import com.example.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,12 @@ import java.util.Optional;
 public class UserService {
     private final UserEntityRepository userEntityRepository;
     private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
 
     //트랜잭션 처리를 해주어 exception이 터지면 rollback 되도록 함
     @Transactional
@@ -31,13 +39,14 @@ public class UserService {
     }
 
     public String login(String userName, String password) {
-        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException());
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s is not found" ,userName)));
 
         // 비밀번호 체크
-        if (!userEntity.getPassword().equals(password)) {
-            throw new SnsApplicationException();
+        if (!encoder.matches(password, userEntity.getPassword())) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
-        return "";
+        String token = JwtTokenUtils.generateToken(userName,secretKey,expiredTimeMs);
+        return token;
     }
 }
