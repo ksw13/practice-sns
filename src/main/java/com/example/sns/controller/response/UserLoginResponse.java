823bd1f8ca40c6f9254e1318cb0bb38565b28325
ksw13 <ksw48483@gmail.com>
2023-04-08 19:16:11 +0900

    DB 연결

--- src/main/java/com/example/sns/controller/response/UserLoginResponse.java
+++ src/main/java/com/example/sns/controller/response/UserLoginResponse.java
@@ -1,7 +1,9 @@
 package com.example.sns.controller.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class UserLoginResponse {
     private String token;
