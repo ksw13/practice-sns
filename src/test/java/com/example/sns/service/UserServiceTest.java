396ba3e6e8a3a7c58f294cf2409d7dd13910ad3d
ksw13 <ksw48483@gmail.com>
2023-04-08 19:19:27 +0900

    UserService 테스트 코드 작성

--- src/test/java/com/example/sns/service/UserServiceTest.java
+++ src/test/java/com/example/sns/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.sns.service;
 
+import com.example.sns.exception.ErrorCode;
 import com.example.sns.exception.SnsApplicationException;
 import com.example.sns.fixture.UserEntityFixture;
 import com.example.sns.model.entity.UserEntity;
@@ -8,16 +9,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@SpringBootApplication
+@SpringBootTest
 public class UserServiceTest {
 
     @Autowired
@@ -38,7 +41,7 @@ public class UserServiceTest {
         // 회원가입이 된적이 없기 때문에 empty() 반환
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
         when(encoder.encode(password)).thenReturn("encrypt_password");
-        when(userEntityRepository.save(any())).thenReturn(Optional.of(mock(UserEntity.class)));
+        when(userEntityRepository.save(any())).thenReturn(UserEntityFixture.get(userName,password));
 
         Assertions.assertDoesNotThrow(() -> userService.join(userName,password));
     }
@@ -47,15 +50,15 @@ public class UserServiceTest {
     void 회원가입시_userName으로_회원가입한적이_있는경우() {
         String userName = "userName";
         String password = "password";
+        UserEntity fixture = UserEntityFixture.get(userName, password);
 
         // mocking
-        // 회원가입이 된적이 없기 때문에 empty() 반환
-        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(mock(UserEntity.class)));
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
         when(encoder.encode(password)).thenReturn("encrypt_password");
-        when(userEntityRepository.save(any())).thenReturn(Optional.of(mock(UserEntity.class)));
+        when(userEntityRepository.save(any())).thenReturn(Optional.of(fixture));
 
         SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, () -> userService.join(userName, password));
-
+        Assertions.assertEquals(ErrorCode.DUPLICATED_USER_NAME, e.getErrorCode());
     }
 
     @Test
@@ -68,6 +71,7 @@ public class UserServiceTest {
         // mocking
         // 회원가입이 된적이 없기 때문에 empty() 반환
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+        when(encoder.matches(password, fixture.getPassword())).thenReturn(true);
 
         Assertions.assertDoesNotThrow(()-> userService.login(userName,password));
     }
@@ -81,7 +85,9 @@ public class UserServiceTest {
         // 회원가입이 된적이 없기 때문에 empty() 반환
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
 
-        Assertions.assertDoesNotThrow(()-> userService.login(userName,password));
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, ()-> userService.login(userName,password));
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, e.getErrorCode());
+
     }
 
     @Test
@@ -96,6 +102,8 @@ public class UserServiceTest {
         // 회원가입이 된적이 없기 때문에 empty() 반환
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
 
-        Assertions.assertThrows(SnsApplicationException.class, ()-> userService.login(userName,wrongPassword));
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, ()-> userService.login(userName,wrongPassword));
+        Assertions.assertEquals(ErrorCode.INVALID_PASSWORD, e.getErrorCode());
+
     }
 }
